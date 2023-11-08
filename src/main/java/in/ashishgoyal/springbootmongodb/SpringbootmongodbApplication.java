0325e4255be9ca462241bc853a9f7de811dad1ce
ashishstudy <ashishstudy120@gmail.com>
2023-11-08 09:36:05 +0530

    adding swagger

--- src/main/java/in/ashishgoyal/springbootmongodb/SpringbootmongodbApplication.java
+++ src/main/java/in/ashishgoyal/springbootmongodb/SpringbootmongodbApplication.java
@@ -3,7 +3,10 @@ package in.ashishgoyal.springbootmongodb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class SpringbootmongodbApplication {
 
 	public static void main(String[] args) {
